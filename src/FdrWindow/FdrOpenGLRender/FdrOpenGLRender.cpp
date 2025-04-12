a69c6504e003676628404bbfc7afb79ca0b3fe52
ralex2304 <ralex2304@gmail.com>
2025-04-12 23:14:06 +0300

    Refactoring after review

--- src/FdrWindow/FdrOpenGLRender/FdrOpenGLRender.cpp
+++ src/FdrWindow/FdrOpenGLRender/FdrOpenGLRender.cpp
@@ -4,11 +4,11 @@
 
 #include "FdrWindow/FdrOpenGLRender/FdrOpenGLRender.hpp"
 
-using namespace FdrOpenGLRender;
+namespace FdrOpenGLRender {
 
 OpenGLRender::OpenGLRender(const std::filesystem::path& vshader_path,
-                           const std::filesystem::path& fshader_path):
-        shader_(vshader_path, fshader_path) {
+                           const std::filesystem::path& fshader_path)
+    : shader_(vshader_path, fshader_path) {
 
     glGenVertexArrays(1, &VAO_);
     glGenBuffers(1, &VBO_);
@@ -17,23 +17,26 @@ OpenGLRender::OpenGLRender(const std::filesystem::path& vshader_path,
     glBindVertexArray(VAO_);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO_);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES_), VERTICES_, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(kVertices_), kVertices_, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO_);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(INDICES_), INDICES_, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(kIndices_), kIndices_, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 }
 
-OpenGLRender::~OpenGLRender() noexcept {
+OpenGLRender::~OpenGLRender() {
     glDeleteVertexArrays(1, &VAO_);
     glDeleteBuffers(1, &VBO_);
     glDeleteBuffers(1, &EBO_);
 }
 
-void OpenGLRender::render() noexcept {
+void OpenGLRender::render() const noexcept {
     shader_.use();
     glBindVertexArray(VAO_);
-    glDrawElements(GL_TRIANGLES, sizeof(INDICES_) / sizeof(*INDICES_), GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, sizeof(kIndices_) / sizeof(*kIndices_), GL_UNSIGNED_INT, 0);
 }
+
+}; //< namespace FdrOpenGLRender
+
